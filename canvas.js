ca5c3659aaf55e14fc394d098b8cd3357ca97f19
gordon-fong <85857853+gordon-fong@users.noreply.github.com>
2021-10-20 13:48:13 +1300

    draw a dot

--- canvas.js
+++ canvas.js
@@ -5,4 +5,19 @@ canvas.height = window.innerHeight
 
 const ctx = canvas.getContext('2d')
 
-console.log(ctx)
+// utility functions
+const randNumBetween = (min, max) => {
+  return (
+    Math.random() * (max - min) + min
+  )
+}
+
+const drawDot = () => {
+  ctx.beginPath()
+  ctx.arc(window.innerWidth/2, window.innerHeight/2, 50, 0, Math.PI*2)
+  ctx.closePath()
+  ctx.fillStyle = 'red'
+  ctx.fill()
+}
+
+drawDot()
