99bcfe3fad42d7821bb648ba89f89626ce376907
gordon-fong <85857853+gordon-fong@users.noreply.github.com>
2021-10-21 01:23:18 +1300

    make rain effect

--- canvas.js
+++ canvas.js
@@ -5,19 +5,71 @@ canvas.height = window.innerHeight
 
 const ctx = canvas.getContext('2d')
 
+let dotArr = []
+let gravity = 1
+
 // utility functions
-const randNumBetween = (min, max) => {
+const randNumBetween = (min, max, isInt) => {
+  const randNum = Math.random() * (max - min) + min
   return (
-    Math.random() * (max - min) + min
+    isInt ? Math.floor(randNum) : randNum
   )
 }
 
-const drawDot = () => {
+const updateDots = () => {
+
+  dotArr.map((dot) => {
+    if (dot.yDir < 0.001 && dot.yDir > -0.001){
+      dot.yPos = 0
+      dot.yDir = randNumBetween(10, 15)
+    }
+
+    if ((dot.yPos + dot.yDir > canvas.height || dot.yPos + dot.yDir < 0) && dot.yDir !== 0) {
+      dot.yDir = -dot.yDir * randNumBetween(0, 0.3)
+    } else {
+      dot.yDir += gravity
+    } 
+
+    dot.yPos += dot.yDir
+
+    drawDot(dot.xPos, dot.yPos, dot.rad)
+  })
+}
+
+const drawDot = (xPos, yPos, rad) => {
   ctx.beginPath()
-  ctx.arc(window.innerWidth/2, window.innerHeight/2, 50, 0, Math.PI*2)
+  ctx.arc(xPos, yPos, rad, 0, Math.PI * 2)
   ctx.closePath()
-  ctx.fillStyle = 'red'
+  ctx.fillStyle = 'teal'
   ctx.fill()
 }
 
-drawDot()
+
+const tick = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    requestAnimationFrame(tick)
+    updateDots()
+}
+
+const init = () => {
+  dotArr = []
+  for (i = 0; i < 500; i++) {
+    const xPos = randNumBetween(0, canvas.width)
+    const yPos = randNumBetween(0, canvas.height)
+    const xDir = 0
+    const yDir = 1
+    const rad = randNumBetween(1, 4)
+
+    const dotData = { xPos, yPos, xDir, yDir, rad }
+
+    dotArr.push(dotData)
+  }
+  // console.log(dotArr)
+  dotArr.map((dot) => {
+    drawDot(dot.xPos, dot.yPos, dot.rad)
+  })
+
+  tick()
+}
+
+init()
